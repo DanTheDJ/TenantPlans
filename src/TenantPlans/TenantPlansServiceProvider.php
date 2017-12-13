64f689701569d8038f877f3fe61e587c7b4540bb
NUMATIC\danc <dan.coleman@numatic.co.uk>
2017-12-13 17:02:54 +0000

    Add initial files

---
+++ src/TenantPlans/TenantPlansServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace DanTheDJ\LaraPlans;
+
+use Illuminate\Support\ServiceProvider;
+
+class TenantPlansServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        //
+    }
+}
